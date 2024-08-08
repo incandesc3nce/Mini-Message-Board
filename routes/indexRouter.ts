2aa9d5893b928e9e838ab0f667b1a770d226fc48
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-08 14:22:11 +0700

    feat: handle post request from message form

--- routes/indexRouter.ts
+++ routes/indexRouter.ts
@@ -17,7 +17,23 @@ const messages: Message[] = [
 const indexRouter: Router = Router();
 
 indexRouter.get('/', (req: Request, res: Response) => {
-  res.render('index', { title: 'Mini Messageboard', messages: messages});
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+indexRouter.post('/new', (req: Request, res: Response) => {
+  const { user, text } = req.body;
+  if (!user || !text) {
+    return res.status(400).send('User and text are required');
+  }
+
+  const newMessage: Message = {
+    user,
+    text,
+    added: new Date(),
+  };
+  messages.unshift(newMessage);
+
+  res.redirect('/');
 });
 
 export default indexRouter;
