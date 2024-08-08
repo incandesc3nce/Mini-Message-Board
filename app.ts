e329d8ef16c0eac120b24ca42d8deb63a9b18863
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-08 13:21:19 +0700

    feat: set views and view engine to app

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import indexRouter from './routes/indexRouter';
+import path from 'path';
 
 const app = express();
 
 app.use('/', indexRouter);
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
 
 app.listen('3000', () => {
   console.log(`Server running at http://localhost:3000`);
