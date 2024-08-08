2adac15024e25e3367ac81e0c59f2d57edd7567f
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-08 14:39:13 +0700

    feat: change file name from "message.ejs" to "newMessage.ejs"

--- routes/messageRouter.ts
+++ routes/messageRouter.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const messageRouter: Router = Router();
 
 messageRouter.get('/', (req: Request, res: Response) => {
-  res.render('message', { title: 'New message' });
+  res.render('newMessage', { title: 'New message' });
 });
 
 export default messageRouter;
