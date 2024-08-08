7b51550ed57eeaeb1e6c042b8ec1c61d11bbbb12
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-08 13:01:55 +0700

    feat: add messages array

--- routes/indexRouter.ts
+++ routes/indexRouter.ts
@@ -1,5 +1,24 @@
 import { Router, Request, Response } from 'express';
 
+type Message = {
+  text: string,
+  user: string,
+  added: Date
+};
+
+const messages: Message[] = [
+  {
+    text: 'Hi there!',
+    user: 'John',
+    added: new Date(),
+  },
+  {
+    text: 'Hello, World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 const indexRouter: Router = Router();
 
 indexRouter.get('/', (req: Request, res: Response) => {
