9b0a4e2a2a86707575cb8d042b025e5254f5b6d5
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-08 17:09:08 +0700

    build: provide an env port

--- app.ts
+++ app.ts
@@ -15,6 +15,8 @@ app.use(express.static(assetsPath));
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
-app.listen('3000', () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:3000`);
 });
