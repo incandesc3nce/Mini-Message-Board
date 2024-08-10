3492b0b945c45afb7ceb33a406126a94a2f2999f
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-08-10 16:53:41 +0700

    feat: export queries

--- db/queries.ts
+++ db/queries.ts
@@ -1,13 +1,15 @@
 import pool from './pool';
 
-export const getMessages = async () => {
+const getMessages = async () => {
   const { rows } = await pool.query('SELECT * FROM messages');
   return rows;
 };
 
-export const getMessage = async (id: string) => {
+const getMessage = async (id: string) => {
   const { rows } = await pool.query('SELECT * FROM messages WHERE id = $1', [
     id,
   ]);
   return rows[0];
 };
+
+export { getMessages, getMessage };
